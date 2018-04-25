3fb537b1b7e063732abf55438b9aa1eda4065193
patchgi <pacchigi0910@gmail.com>
2018-04-25 17:53:40 +0900

    Initial Commit

---
+++ JINS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JINS
+//
+//  Created by 八木康輔 on 2018/04/25.
+//  Copyright © 2018年 patchgi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
