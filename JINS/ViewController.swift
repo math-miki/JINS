66169803887187121096c1c92cc607d7451151d7
patchgi <pacchigi0910@gmail.com>
2018-04-28 17:31:56 +0900

    init

--- JINS/ViewController.swift
+++ JINS/ViewController.swift
@@ -7,19 +7,116 @@
 //
 
 import UIKit
+import Foundation
+import Darwin
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, MEMELibDelegate {
+    
+    @IBOutlet weak var accX: UILabel!
+    @IBOutlet weak var accY: UILabel!
+    @IBOutlet weak var accZ: UILabel!
+    
+    @IBOutlet weak var yaw: UILabel!
+    @IBOutlet weak var roll: UILabel!
+    @IBOutlet weak var pitch: UILabel!
+    
+    @IBOutlet weak var eyeMoveUp: UILabel!
+    @IBOutlet weak var eyeMoveDown: UILabel!
+    @IBOutlet weak var eyeMoveLeft: UILabel!
+    @IBOutlet weak var eyeMoveRight: UILabel!
+    
+    @IBOutlet weak var blinkStrength: UILabel!
+    @IBOutlet weak var blinkSpeed: UILabel!
+    
+    @IBOutlet weak var isWalking: UILabel!
+    
+    @IBOutlet weak var powerLeft: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        MEMELib.sharedInstance().delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func memeAppAuthorized(_ status: MEMEStatus) {
+        MEMELib.sharedInstance().startScanningPeripherals()
+        self.checkMEMEStatus(status)
+    }
+    
+    func memePeripheralFound(_ peripheral: CBPeripheral!, withDeviceAddress address: String!) {
+        MEMELib.sharedInstance().connect(peripheral)
+    }
+    
+    func memePeripheralConnected(_ peripheral: CBPeripheral!) {
+        let status = MEMELib.sharedInstance().startDataReport()
+        print(status)
     }
 
-
+    
+    func memeRealTimeModeDataReceived(_ data: MEMERealTimeData!) {
+        let row_sensor_data = data.description.components(separatedBy: "{")[1].components(separatedBy: "}")[0].components(separatedBy: ";").map{$0.replacingOccurrences(of:" ", with:"")}
+        var sensor_data = Dictionary<String, String>()
+        for _datam in row_sensor_data{
+            let datam = _datam.components(separatedBy: "=")
+            if (datam.count > 1){
+                let k = datam[0]
+                let v = (datam[1])
+                sensor_data[k] = v
+            }
+        }
+        print(sensor_data)
+        
+        accX.text = "accX: " + sensor_data["accX"]!
+        accY.text = "accY: " + sensor_data["accY"]!
+        accZ.text = "accZ: " + sensor_data["accZ"]!
+        
+        yaw.text = "yaw: " + sensor_data["yaw"]!
+        roll.text = "roll: " + sensor_data["roll"]!
+        pitch.text = "pitch: " + sensor_data["pitch"]!
+        
+        eyeMoveUp.text = "eyeMoveUp: " + sensor_data["eyeMoveUp"]!
+        eyeMoveDown.text = "eyeMoveDown: " + sensor_data["eyeMoveDown"]!
+        eyeMoveLeft.text = "eyeMoveLeft: " + sensor_data["eyeMoveLeft"]!
+        eyeMoveRight.text = "eyeMoveRight: " + sensor_data["eyeMoveRight"]!
+        
+        blinkStrength.text = "blinkStrength: " + sensor_data["blinkStrength"]!
+        blinkSpeed.text = "blinkSpeed: " + sensor_data["blinkSpeed"]!
+        
+        isWalking.text = "isWalking: " + sensor_data["isWalking"]!
+        
+        powerLeft.text = "powerLeft: " + sensor_data["powerLeft"]!
+        
+        
+    }
+    func checkMEMEStatus(_ status:MEMEStatus) {
+        
+        if status == MEME_ERROR_APP_AUTH {
+            let alertController = UIAlertController(title: "App Auth Failed", message: "Invalid Application ID or Client Secret ", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default, handler:nil)
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
+        } else if status == MEME_ERROR_SDK_AUTH{
+            let alertController = UIAlertController(title: "SDK Auth Failed", message: "Invalid SDK. Please update to the latest SDK.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default, handler:nil)
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
+        } else if status == MEME_CMD_INVALID {
+            let alertController = UIAlertController(title: "SDK Error", message: "Invalid Command", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default, handler:nil)
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
+        } else if status == MEME_ERROR_BL_OFF {
+            let alertController = UIAlertController(title: "Error", message: "Bluetooth is off.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default, handler:nil)
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
+        } else if status == MEME_OK {
+            print("Status: MEME_OK")
+        }
+    }
 }
 
---
+++ JINS/jins_test.h
@@ -0,0 +1,13 @@
+//
+//  jins_test.h
+//  JINS
+//
+//  Created by 八木康輔 on 2018/04/25.
+//  Copyright © 2018年 patchgi. All rights reserved.
+//
+
+#ifndef jins_test_h
+#define jins_test_h
+#import <MEMELib/MEMELib.h>
+
+#endif /* jins_test_h */
